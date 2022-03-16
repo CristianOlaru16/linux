379e3df42bb1a729a7de979eb248f1b0c1fd28ec
Cristian Olaru <cristianolaru99@gmail.com>
2022-03-16 02:06:46 +0200

    lab 1: ex 7
    Signed-off-by: Cristian Olaru <cristianolaru99@gmail.com>

--- tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
+++ tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 /* TODO: add missing headers */
+#include <linux/sched/signal.h>
 
 MODULE_DESCRIPTION("List current processes");
 MODULE_AUTHOR("Kernel Hacker");
@@ -13,7 +14,12 @@ static int my_proc_init(void)
 
 	/* TODO: print current process pid and its name */
 
+	pr_info("Current process: pid = %d, name = %s\n", current->pid, current->comm);
+
 	/* TODO: print the pid and name of all processes */
+	for_each_process(p) {
+		pr_info("pid = %d, name = %s\n", p->pid, p->comm);
+	}
 
 	return 0;
 }
@@ -21,6 +27,8 @@ static int my_proc_init(void)
 static void my_proc_exit(void)
 {
 	/* TODO: print current process pid and name */
+	
+	pr_info("Current process: pid = %d, name = %s\n", current->pid, current->comm);
 }
 
 module_init(my_proc_init);
