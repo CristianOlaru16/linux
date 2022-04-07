f29ca44ed3565ec170000cfb623093eb5bceb7e6
Cristian Olaru <cristianolaru99@gmail.com>
2022-04-07 23:00:43 +0300

    lab 5: ex 7
    Signed-off-by: Cristian Olaru <cristianolaru99@gmail.com>

--- tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
+++ tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
@@ -47,7 +47,11 @@ static struct my_device_data {
 	struct work_struct work;
 
 	/* TODO 4: add list for monitored processes */
+	struct list_head list;
+
 	/* TODO 4: add spinlock to protect list */
+	spinlock_t lock;
+
 } dev;
 
 static void alloc_io(void)
@@ -108,6 +112,26 @@ static void timer_handler(struct timer_list *tl)
 #endif
 			break;
 
+		case TIMER_TYPE_MON:
+		{
+			struct mon_proc *p, *n;
+			
+			spin_lock(&my_data->lock);
+			list_for_each_entry_safe(p, n, &my_data->list, list) {
+				if (p->task->state == TASK_DEAD) {
+					pr_info("task %s (%d) is dead\n", p->task->comm,
+						p->task->pid);
+					put_task_struct(p->task);
+					list_del(&p->list);
+					kfree(p);
+				}
+			}
+
+			spin_unlock(&my_data->lock);
+
+			mod_timer(&my_data->timer, jiffies + HZ);
+			break;
+		}
 		default:
 			break; 
 	}
@@ -159,9 +183,19 @@ static long deferred_ioctl(struct file *file, unsigned int cmd, unsigned long ar
 		case MY_IOCTL_TIMER_MON:
 		{
 			/* TODO 4: use get_proc() and add task to list */
+			struct mon_proc *p = get_proc(arg);
+			if (IS_ERR(p))
+				return PTR_ERR(p);
+
 			/* TODO 4: protect access to list */
+			spin_lock_bh(&my_data->lock);
+			list_add(&p->list, &my_data->list);
+			spin_unlock_bh(&my_data->lock);
 
 			/* TODO 4: set flag and schedule timer */
+			my_data->flag = TIMER_TYPE_MON;
+			mod_timer(&my_data->timer, jiffies + HZ);
+
 			break;
 		}
 		default:
@@ -195,6 +229,8 @@ static int deferred_init(void)
 	INIT_WORK(&dev.work, work_handler);
 
 	/* TODO 4: Initialize lock and list. */
+	spin_lock_init(&dev.lock);
+	INIT_LIST_HEAD(&dev.list);
 
 	cdev_init(&dev.cdev, &my_fops);
 	cdev_add(&dev.cdev, MKDEV(MY_MAJOR, MY_MINOR), 1);
@@ -224,6 +260,11 @@ static void deferred_exit(void)
 		/* TODO 4: ... decrement task usage counter ... */
 		/* TODO 4: ... remove it from the list ... */
 		/* TODO 4: ... free the struct mon_proc */
+	list_for_each_entry_safe(p, n, &dev.list, list) {
+		put_task_struct(p->task);
+		list_del(&p->list);
+		kfree(p);
+	}
 }
 
 module_init(deferred_init);
